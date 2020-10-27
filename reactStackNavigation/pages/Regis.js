2403d4ce7e534a9a80d718229cf67fa0bf3fd81b
CindoddCindy <cindodcindy@gmail.com>
2020-10-27 14:35:11 +0700

    belajar react

--- reactStackNavigation/pages/Regis.js
+++ reactStackNavigation/pages/Regis.js
@@ -1,18 +1,88 @@
-import * as React from 'react';
+//import * as React from 'react';
 
-import {Button, View, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, View,Text,SafeAreaView,TextInput} from 'react-native';
+
+import React, {useState} from 'react';
+
 
 const Regis = ({navigation}) =>{
+    
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userPhoneNumb, setUserPhoneNumb] =useState ('');
+    const [userBirthDate, setUserBirthDate] =useState ('');
+    const [userPassword, setUserPassword] = useState('');
+    const [userPassConf, setUSerPassConf] = useState('');
+    
     return(
-        <SafeAreaView>
-            <View>
-                <Text>
-                    Register User
-                </Text>
-            </View>
-        </SafeAreaView>
 
+        <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text>Insert any text in below input</Text>
+        <TextInput
+          value={userName}
+          onChangeText={(userName) => setUserName(userName)}
+          placeholder={'UserName'}
+          style={styles.input}
+        />
+        <Text style={{color: 'blue'}}>{userName}</Text>
+        <Text style={{color: 'purple'}}>{userName}</Text>
+<TextInput
+value={userEmail}
+onChangeText={(userEmail) => setUserEmail(userEmail)}
+placeholder={'UserEmail'}
+style={styles.input}
+/>
+<Text style={{color: 'purple'}}>{userEmail}</Text>
+<TextInput
+value={userBirthDate}
+onChangeText={(userBirthDate) => setUserBirthDate(userBirthDate)}
+placeholder={'UserBirthDate'}
+style={styles.input}
+/>
+<Text style={{color: 'purple'}}>{userBirthDate}</Text>
+<TextInput
+value={userPhoneNumb}
+onChangeText={(userPhoneNumb) => setUserPhoneNumb(userPhoneNumb)}
+placeholder={'UserPhoneNumb'}
+style={styles.input}
+/>
+<Text style={{color: 'purple'}}>{userPhoneNumb}</Text>
+<TextInput
+value={userPassword}
+onChangeText={(userPassword) => setUserPassword(userPassword)}
+placeholder={'UserPassword'}
+style={styles.input}
+/>
+<Text style={{color: 'purple'}}>{userPassword}</Text>
+<TextInput
+value={userPassConf}
+onChangeText={(userPassConf) => setUSerPassConf(userPassConf)}
+placeholder={'UserPassConf'}
+style={styles.input}
+/>
+<Text style={{color: 'purple'}}>{userPassConf}</Text>
+ </View>
+ </SafeAreaView>
+      
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      marginTop: 20,
+      backgroundColor: '#ffffff',
+    },
+    input: {
+      width: 250,
+      height: 44,
+      padding: 10,
+      marginTop: 20,
+      marginBottom: 10,
+      backgroundColor: '#e8e8e8'
+    },
+  });
 
 export default Regis;
