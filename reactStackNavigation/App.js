bb9096e44a0fbb0000bc6e9e93ba618a0d55d948
CindoddCindy <cindodcindy@gmail.com>
2020-10-28 15:04:31 +0700

    kacau

--- reactStackNavigation/App.js
+++ reactStackNavigation/App.js
@@ -2,16 +2,137 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 
+
+import{MaterialCommunityIcons} from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+
 import {Button, View, Text} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 
 import {createStackNavigator} from '@react-navigation/stack';
 
+import React from 'react';
+import {
+  Button,
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+//import all the basic component we have used
+import Ionicons from 'react-native-vector-icons/Ionicons';
+//import Ionicons to show the icon for bottom options
+
+//import React Navigation
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+
+
+
+
+
 import Login from './pages/Login';
 import Regis from './pages/Regis';
 
+
+import Home from './bottomnav/Home';
+import Booking from './bottomnav/Booking';
+import Inbox from './bottomnav/Inbox';
+import Profile from './bottomnav/Profile';
+
+
+
+
+const Tab = createBottomTabNavigator();
+
 const Stack = createStackNavigator();
+
+const Home = createStackNavigator(
+  {
+    //Defination of Navigaton from home screen
+    Home: { screen: Home },
+    Details: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      //Header customization of the perticular Screen
+      headerStyle: {
+        backgroundColor: '#42f44b',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'Home',
+      //Header title
+    },
+  }
+);
+const Booking = createStackNavigator(
+  {
+    //Defination of Navigaton from setting screen
+    Booking: { screen: Booking },
+    Details: { screen: Booking },
+   
+  },
+  {
+    defaultNavigationOptions: {
+      //Header customization of the perticular Screen
+      headerStyle: {
+        backgroundColor: '#42f44b',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'Settings',
+      //Header title
+    },
+  }
+);
+
+const Inbox = createStackNavigator(
+  {
+    //Defination of Navigaton from setting screen
+    Booking: { screen: Inbox },
+    Details: { screen: Inbox },
+   
+  },
+  {
+    defaultNavigationOptions: {
+      //Header customization of the perticular Screen
+      headerStyle: {
+        backgroundColor: '#42f44b',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'Settings',
+      //Header title
+    },
+  }
+);
+
+const Profile = createStackNavigator(
+  {
+    //Defination of Navigaton from setting screen
+    Booking: { screen: Profile },
+    Details: { screen: Profile },
+   
+  },
+  {
+    defaultNavigationOptions: {
+      //Header customization of the perticular Screen
+      headerStyle: {
+        backgroundColor: '#42f44b',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'Settings',
+      //Header title
+    },
+  }
+);
+
+
+
+
+
 
 function App(){
   return(
@@ -49,6 +170,70 @@ function App(){
 
       </Stack.Navigator>
     </NavigationContainer>
+
+
+
+
+//bottom nav 
+
+
+
+
+
   );
 }
+
+
+
 export default App;
+
+
+
+const App = createBottomTabNavigator(
+  {
+    Home: { screen: Home },
+    Booking: { screen: Booking },
+    Inbox: {screen : Inbox},
+    Profile: {screen : Profile},
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ?
+            '' : '-outline'
+          }`;
+        } else if (routeName === 'Booking') {
+          iconName = `ios-checkmark-circle${focused ?
+            '' : '-outline'
+          }`;
+        }
+        else if (routeName === 'Inbox'){
+          iconName = `ios-checkmark-circle${focused ?
+            '' : '-outline'
+          }`;
+          
+        }
+        else if (routeName === 'Profile'){
+          iconName = `ios-checkmark-circle${focused ?
+            '' : '-outline'
+          }`;
+
+        }
+        return <IconComponent
+                 name={iconName}
+                 size={25}
+                 color={tintColor}
+               />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#42f44b',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
--- reactStackNavigation/pages/Regis.js
+++ reactStackNavigation/pages/Regis.js
@@ -30,49 +30,53 @@ const Regis = ({navigation}) =>{
       />
    
         <Text> Regis Here</Text>
+        <Text style={styles.title}>Input Name</Text>
         <TextInput
           value={userName}
           onChangeText={(userName) => setUserName(userName)}
           placeholder={'UserName'}
           style={styles.input}
         />
-        <Text style={{color: 'blue'}}>{userName}</Text>
-        <Text style={{color: 'purple'}}>{userName}</Text>
+         <Text style={styles.title}>Input Email</Text>
         <TextInput
         value={userEmail}
         onChangeText={(userEmail) => setUserEmail(userEmail)}
         placeholder={'UserEmail'}
         style={styles.input}
         />
-        <Text style={{color: 'purple'}}>{userEmail}</Text>
+         <Text style={styles.title}>Input Birth Date</Text>
         <TextInput
         value={userBirthDate}
         onChangeText={(userBirthDate) => setUserBirthDate(userBirthDate)}
         placeholder={'UserBirthDate'}
         style={styles.input}
         />
-        <Text style={{color: 'purple'}}>{userBirthDate}</Text>
+       
+       <Text style={styles.title}>Input Phone Number</Text>
         <TextInput
         value={userPhoneNumb}
         onChangeText={(userPhoneNumb) => setUserPhoneNumb(userPhoneNumb)}
         placeholder={'UserPhoneNumb'}
         style={styles.input}
         />
-        <Text style={{color: 'purple'}}>{userPhoneNumb}</Text>
+       
+       <Text style={styles.title}>Input Password</Text>
         <TextInput
         value={userPassword}
         onChangeText={(userPassword) => setUserPassword(userPassword)}
         placeholder={'UserPassword'}
         style={styles.input}
         />
-        <Text style={{color: 'purple'}}>{userPassword}</Text>
+         
+         <Text style={styles.title}>Input Password Confirmation</Text>
+
         <TextInput
         value={userPassConf}
         onChangeText={(userPassConf) => setUSerPassConf(userPassConf)}
         placeholder={'UserPassConf'}
         style={styles.input}
         />
-        <Text style={{color: 'purple'}}>{userPassConf}</Text>
+       
 
         <Button 
         onPress={onPressLearnMore}
@@ -97,12 +101,20 @@ const styles = StyleSheet.create({
       backgroundColor: '#ffffff',
     },
     input: {
-      width: 250,
+      width: 350,
       height: 44,
       padding: 10,
-      marginTop: 20,
+      marginTop: 10,
       marginBottom: 10,
       backgroundColor: '#e8e8e8'
+    },
+    title: {
+      fontSize: 12,
+      alignItems: 'center',
+      fontWeight: 'bold',
+      color: 'purple',
+      marginTop: 20,
+      
     },
   });
 
